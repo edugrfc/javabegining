636405d8effc3815c04eba003b7d714021f94371
Andrey Nasonov <nag.grfc@mail.ru>
2018-10-28 22:40:07 +0300

    AWT 1c - закрываемое окно с Adapter

--- a.java
+++ a.java
@@ -3,49 +3,27 @@ import java.awt.event.*;
 
 public class a {
 	
-    public static class MainWindow extends Frame implements WindowListener {
+    public static class MainWindow extends Frame {
+            
+        public MainWindow() {
+            WindowListener listener = new MainWindowListener();
+            addWindowListener(listener);
+        }
+            
+        private class MainWindowListener extends WindowAdapter {
 	
-        @Override
-        public void windowActivated(WindowEvent e) {
-            System.out.println("windowActivated");
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+            
         }
 
-        @Override
-        public void windowClosed(WindowEvent e) {
-            System.out.println("windowClosed");
-        }
-
-        @Override
-        public void windowClosing(WindowEvent e) {
-            System.out.println("windowClosing");
-            //dispose();
-        }
-
-        @Override
-        public void windowDeactivated(WindowEvent e) {
-            System.out.println("windowDeactivated");
-        }
-
-        @Override
-        public void windowDeiconified(WindowEvent e) {
-            System.out.println("windowDeiconified");
-        }
-
-        @Override
-        public void windowIconified(WindowEvent e) {
-            System.out.println("windowIconified");
-        }
-
-        @Override
-        public void windowOpened(WindowEvent e) {
-            System.out.println("windowOpened");
-        }
     }
 
     public static void main(String args[]) {
         System.out.println("Hello, GRFC!");
         Frame f = new MainWindow();
-        f.addWindowListener(f);
         f.setSize(300, 300);
         f.setVisible(true);
     }

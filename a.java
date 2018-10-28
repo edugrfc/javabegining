f435af23a901149716efa78edf556cc883f74aa7
Andrey Nasonov <nag.grfc@mail.ru>
2018-10-28 21:30:49 +0300

    AWT 1 - незакрываемое окно

--- a.java
+++ a.java
@@ -1,7 +1,13 @@
-public class a {
+import java.awt.*;
+
+public class a extends Frame {
 
     public static void main(String args[]) {
         System.out.println("Hello, GRFC!");
+		Frame f = new a();
+		f.setSize(300, 300);
+		f.setVisible(true);
+		//f.setSize(300, 300);
     }
-
+	
 }

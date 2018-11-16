ce341610699b45cef06b757d3079f58a77fc7850
MDAgrfc <dm.makarov@int.grfc.ru>
2018-11-16 11:38:57 +0300

    homework 01.1

---
+++ edu_awt/EduAwtHello/src/ru/grfc/edu/awthello/MyController.java
@@ -0,0 +1,47 @@
+
+import java.awt.event.*;
+import java.awt.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mada
+ */
+public class MyController extends WindowAdapter{
+    
+    
+    Frame frame;
+    
+    
+    MyController(Frame e){
+    
+        this.frame=e;
+    
+    
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        super.windowClosing(e); //To change body of generated methods, choose Tools | Templates.
+        frame.dispose();
+        
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        super.windowClosed(e); //To change body of generated methods, choose Tools | Templates.
+        frame.dispose();
+    }
+    
+    
+    
+    
+    
+    
+    
+}

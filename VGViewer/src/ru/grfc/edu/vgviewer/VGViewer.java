bdc8b1e8333cf097241d114cfa96fbc6b0c824f2
Andrey Nasonov <nag.grfc@mail.ru>
2018-11-29 21:17:41 +0300

    Выделен метод генерации фигур

--- VGViewer/src/ru/grfc/edu/vgviewer/VGViewer.java
+++ VGViewer/src/ru/grfc/edu/vgviewer/VGViewer.java
@@ -2,12 +2,14 @@ package ru.grfc.edu.vgviewer;
 
 import java.awt.Canvas;
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import ru.grfc.edu.vgviewer.figures.Coordinate;
+import ru.grfc.edu.vgviewer.figures.Figure;
 import ru.grfc.edu.vgviewer.figures.Line;
 import ru.grfc.edu.vgviewer.figures.Parallelogram;
 import ru.grfc.edu.vgviewer.figures.Rectangle;
@@ -31,77 +33,101 @@ public class VGViewer {
             }
         });
 
-        Canvas canvas = new MyCanvas();
+        MyCanvas canvas = new MyCanvas();
         frame.add(canvas);
         frame.setVisible(true);
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<Figure> figures = generateFigures();
+                canvas.setFigures(figures);
+                canvas.repaint();
+            }
+        });
     }
 
     private static class MyCanvas extends Canvas {
+
+        private ArrayList<Figure> figures;
+
+        public void setFigures(ArrayList<Figure> figures) {
+            this.figures = figures;
+        }
 
         @Override
         public void paint(Graphics g) {
             int width = getWidth();
             int height = getHeight();
-
-            //для примера нарисуем что-то похожее на блок-схему
-            //терминатор начала
-            RoundRectangle rr = new RoundRectangle(70, 30, 20, 20,
-                    new Coordinate((width / 2) - 30, 10), Color.RED, false);
-            rr.draw(g);
-            Line line1 = new Line(new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 2,
-                    10 + rr.getHeight()), new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 2,
-                    10 + rr.getHeight() + 30), Color.red);
-            line1.draw(g);
-
-            //блок ввода
-            Parallelogram p = new Parallelogram(true, rr.getWidth(), rr.getHeight(),
-                    new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 10,
-                            line1.getLastPoint().getY()), Color.red, false);
-            p.draw(g);
-            Line line2 = new Line(new Coordinate(line1.getFirstPoint().getX(),
-                    line1.getLastPoint().getY() + p.getHeight()),
-                    new Coordinate(line1.getFirstPoint().getX(),
-                            line1.getLastPoint().getY() + p.getHeight() + 30), Color.red);
-            line2.draw(g);
-
-            //блок операций
-            Rectangle r = new Rectangle(rr.getWidth(), rr.getHeight(),
-                    new Coordinate(rr.getFirstPoint().getX(), line2.getLastPoint().getY()),
-                    Color.red, false);
-            r.draw(g);
-            Line line3 = new Line(new Coordinate(line1.getFirstPoint().getX(),
-                    line2.getLastPoint().getY() + r.getHeight()),
-                    new Coordinate(line1.getFirstPoint().getX(),
-                            line2.getLastPoint().getY() + r.getHeight() + 30), Color.red);
-            line3.draw(g);
-
-            //блок ветвления, но само ветвление не рисуем
-            Rhombus rb = new Rhombus(r.getWidth(), new Coordinate(line3.getLastPoint().getX(),
-                    line3.getLastPoint().getY()), Color.red, false);
-            rb.draw(g);
-            Line line4 = new Line(new Coordinate(line1.getFirstPoint().getX(),
-                    line3.getLastPoint().getY() + rb.getWidth() / 2),
-                    new Coordinate(line1.getFirstPoint().getX(),
-                            line3.getLastPoint().getY() + rb.getWidth() / 2 + 30), Color.red);
-            line4.draw(g);
-
-            //блок операций
-            Rectangle r2 = new Rectangle(rr.getWidth(), rr.getHeight(),
-                    new Coordinate(rr.getFirstPoint().getX(), line4.getLastPoint().getY()),
-                    Color.red, false);
-            r2.draw(g);
-            Line line5 = new Line(new Coordinate(line1.getFirstPoint().getX(),
-                    line4.getLastPoint().getY() + r2.getHeight()),
-                    new Coordinate(line1.getFirstPoint().getX(),
-                            line4.getLastPoint().getY() + r2.getHeight() + 30), Color.red);
-            line5.draw(g);
-
-            //терминатор конца
-            RoundRectangle rr2 = new RoundRectangle(rr.getWidth(), rr.getHeight(), rr.getArcHeight(), rr.getArcWidth(),
-                    new Coordinate(rr.getFirstPoint().getX(), line5.getLastPoint().getY()), Color.RED, false);
-            rr2.draw(g);
-
+            for (Figure figure : figures) {
+                figure.draw(g);
+            }
         }
+    }
+
+    private static ArrayList<Figure> generateFigures() {
+
+        ArrayList<Figure> figures = new ArrayList<>();
+        int width = 300;
+
+        //для примера нарисуем что-то похожее на блок-схему
+        //терминатор начала
+        RoundRectangle rr = new RoundRectangle(70, 30, 20, 20,
+                new Coordinate((width / 2) - 30, 10), Color.RED, false);
+        figures.add(rr);
+        Line line1 = new Line(new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 2,
+                10 + rr.getHeight()), new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 2,
+                10 + rr.getHeight() + 30), Color.red);
+        figures.add(line1);
+
+        //блок ввода
+        Parallelogram p = new Parallelogram(true, rr.getWidth(), rr.getHeight(),
+                new Coordinate(rr.getFirstPoint().getX() + rr.getWidth() / 10,
+                        line1.getLastPoint().getY()), Color.red, false);
+        figures.add(p);
+        Line line2 = new Line(new Coordinate(line1.getFirstPoint().getX(),
+                line1.getLastPoint().getY() + p.getHeight()),
+                new Coordinate(line1.getFirstPoint().getX(),
+                        line1.getLastPoint().getY() + p.getHeight() + 30), Color.red);
+        figures.add(line2);
+
+        //блок операций
+        Rectangle r = new Rectangle(rr.getWidth(), rr.getHeight(),
+                new Coordinate(rr.getFirstPoint().getX(), line2.getLastPoint().getY()),
+                Color.red, false);
+        figures.add(r);
+        Line line3 = new Line(new Coordinate(line1.getFirstPoint().getX(),
+                line2.getLastPoint().getY() + r.getHeight()),
+                new Coordinate(line1.getFirstPoint().getX(),
+                        line2.getLastPoint().getY() + r.getHeight() + 30), Color.red);
+        figures.add(line3);
+
+        //блок ветвления, но само ветвление не рисуем
+        Rhombus rb = new Rhombus(r.getWidth(), new Coordinate(line3.getLastPoint().getX(),
+                line3.getLastPoint().getY()), Color.red, false);
+        figures.add(rb);
+        Line line4 = new Line(new Coordinate(line1.getFirstPoint().getX(),
+                line3.getLastPoint().getY() + rb.getWidth() / 2),
+                new Coordinate(line1.getFirstPoint().getX(),
+                        line3.getLastPoint().getY() + rb.getWidth() / 2 + 30), Color.red);
+        figures.add(line4);
+
+        //блок операций
+        Rectangle r2 = new Rectangle(rr.getWidth(), rr.getHeight(),
+                new Coordinate(rr.getFirstPoint().getX(), line4.getLastPoint().getY()),
+                Color.red, false);
+        figures.add(r2);
+        Line line5 = new Line(new Coordinate(line1.getFirstPoint().getX(),
+                line4.getLastPoint().getY() + r2.getHeight()),
+                new Coordinate(line1.getFirstPoint().getX(),
+                        line4.getLastPoint().getY() + r2.getHeight() + 30), Color.red);
+        figures.add(line5);
+
+        //терминатор конца
+        RoundRectangle rr2 = new RoundRectangle(rr.getWidth(), rr.getHeight(), rr.getArcHeight(), rr.getArcWidth(),
+                new Coordinate(rr.getFirstPoint().getX(), line5.getLastPoint().getY()), Color.RED, false);
+        figures.add(rr2);
+
+        return figures;
     }
 
 }

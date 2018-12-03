21a0d52b1e7f9ae0fb2908f3e7c9a27bc8dcea9f
MDAgrfc <dm.makarov@int.grfc.ru>
2018-12-03 09:58:07 +0300

    added button and choice classes

---
+++ VGViewer/src/ru/grfc/edu/vgviewer/MyButton.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.grfc.edu.vgviewer;
+
+import java.awt.Button;
+import javax.swing.event.MouseInputListener;
+
+/**
+ *
+ * @author mada
+ */
+public class MyButton extends Button {
+    
+}
+
+
+class MyMouseListener extends MouseInputListener{
+
+}
---
+++ VGViewer/src/ru/grfc/edu/vgviewer/MyChoice.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.grfc.edu.vgviewer;
+
+import java.awt.Choice;
+
+/**
+ *
+ * @author mada
+ */
+public class MyChoice extends Choice {
+
+    MyChoice() {
+
+        super();
+        super.add("Rectangle");
+        super.add("Square");
+        super.add("Line");
+        super.add("Ellipse");
+        super.add("Circle");
+
+    }
+
+}

6f884ac5421df271cf8be8efcc6fccbac765448a
Andrey Nasonov <nag.grfc@mail.ru>
2018-12-05 14:49:44 +0300

    refactor

--- edu_files/EduFiles/src/EduFiles7InputSer.java
+++ edu_files/EduFiles/src/EduFiles7InputSer.java
@@ -1,34 +1,28 @@
 
 import java.io.*;
-import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class EduFiles6Serializable {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-    static final List<Order> orders = new ArrayList();
-    
-    static {
-        orders.add(new Order("Java T-shirt", 19.99, 12));
-        orders.add(new Order("Java Mug", 9.99, 8));
-        orders.add(new Order("Duke Juggling Dolls", 15.99, 13));
-        orders.add(new Order("Java Pin", 3.99, 29));
-        orders.add(new Order("Java Key Chain",4.99, 50));
-    }
-    
+/**
+ *
+ * @author nag
+ */
+public class EduFiles7InputSer {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         String dataFile = "object.bin";
         //ObjectOutputStream
         //ObjectInputStream
         
-        try ( ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(dataFile)));) {
-            for (Order order : orders) {
-                out.writeObject(order);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(EduFiles6Serializable.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
         try ( ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream(dataFile)));) {
 
             double total = 0.0;
@@ -45,11 +39,10 @@ public class EduFiles6Serializable {
             System.out.format("Total: " + " $%.2f%n", total);
             
         } catch (ClassNotFoundException ex) {
-                Logger.getLogger(EduFiles6Serializable.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EduFiles7InputSer.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(EduFiles6Serializable.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
     
 }
